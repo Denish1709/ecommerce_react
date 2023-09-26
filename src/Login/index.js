f0433bc39749e9b5469824b5fe358b42dae15b77
Denish1709 <denishrao22s@forward.edu.my>
2023-09-26 10:55:19 +0800

    update(26/09/2023){2}

--- src/Login/index.js
+++ src/Login/index.js
@@ -1,4 +1,13 @@
-import { Container, Title, Space, Divider } from "@mantine/core";
+import {
+  Container,
+  Title,
+  Space,
+  Divider,
+  TextInput,
+  Card,
+  Group,
+  Button,
+} from "@mantine/core";
 import Header from "../Header";
 
 export default function Login() {
@@ -9,6 +18,25 @@ export default function Login() {
         <Title align="center">Login</Title>
         <Header />
         <Space h="30px" />
+        <Card withBorder shadow="md" p="20px">
+          <TextInput
+            placeholder="Enter the Username here"
+            label="Name"
+            description="The username"
+          />
+          <Space h="20px" />
+          <Divider />
+          <Space h="20px" />
+          <TextInput
+            placeholder="Enter the Password here"
+            label="Password"
+            description="Password"
+          />
+        </Card>
+        <Space h="20px" />
+        <Group>
+          <Button fullWidth>Login</Button>
+        </Group>
       </Container>
     </div>
   );
--- src/Signup/index.js
+++ src/Signup/index.js
@@ -1,7 +1,16 @@
-import { Container, Title, Space, Divider } from "@mantine/core";
+import {
+  Container,
+  Title,
+  Space,
+  Divider,
+  TextInput,
+  Card,
+  Button,
+  Group,
+} from "@mantine/core";
 import Header from "../Header";
 
-export default function Login() {
+export default function Signup() {
   return (
     <div className="App">
       <Container>
@@ -9,6 +18,41 @@ export default function Login() {
         <Title align="center">Sign Up</Title>
         <Header />
         <Space h="30px" />
+        <Card withBorder shadow="md" p="20px">
+          <TextInput
+            placeholder="Enter the Username here"
+            label="Name"
+            description="The username"
+          />
+          <Space h="20px" />
+          <Divider />
+          <Space h="20px" />
+          <TextInput
+            placeholder="Enter the Email here"
+            label="Email"
+            description="Email"
+          />
+          <Space h="20px" />
+          <Divider />
+          <Space h="20px" />
+          <TextInput
+            placeholder="Enter the Password here"
+            label="Password"
+            description="Password"
+          />
+          <Space h="20px" />
+          <Divider />
+          <Space h="20px" />
+          <TextInput
+            placeholder="Confirm your password here"
+            label="Confirm Password"
+            description="Confirm Password"
+          />
+        </Card>
+        <Space h="20px" />
+        <Group>
+          <Button fullWidth>Register</Button>
+        </Group>
       </Container>
     </div>
   );
