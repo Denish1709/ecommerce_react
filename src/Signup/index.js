e5aa29846eebbf1c3fb6dd4d4978a709383e239d
Denish1709 <denishrao22s@forward.edu.my>
2023-09-26 10:37:29 +0800

    update(26/09/2023){1}

---
+++ src/Signup/index.js
@@ -0,0 +1,15 @@
+import { Container, Title, Space, Divider } from "@mantine/core";
+import Header from "../Header";
+
+export default function Login() {
+  return (
+    <div className="App">
+      <Container>
+        <Space h="50px" />
+        <Title align="center">Sign Up</Title>
+        <Header />
+        <Space h="30px" />
+      </Container>
+    </div>
+  );
+}
